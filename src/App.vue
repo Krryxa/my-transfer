4916dd1cd8374e83e01350b809a18bcba158efb3
Krryxa <krryxq@163.com>
2019-11-01 17:42:10 +0800

    feat: add images

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 a {
   text-decoration: none;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import krryTransfer from 'krry-transfer'
