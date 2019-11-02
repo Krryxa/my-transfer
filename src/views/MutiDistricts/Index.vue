d85375de351ebfdfd6c1a87ce9df883412a46b4a
krry <krryxq@163.com>
2019-11-02 14:23:59 +0800

    feat: tst

--- src/views/MutiDistricts/Index.vue
+++ src/views/MutiDistricts/Index.vue
@@ -4,7 +4,7 @@
     <el-form>
       <!-- 仓库 -->
       <el-form-item label="地域：">
-        <krry-transfer :districtListList="districtList" :checkedData="checkedData"></krry-transfer>
+        <krry-transfer :dataObj="dataObj" :selectedData="selectedData"></krry-transfer>
       </el-form-item>
     </el-form>
     <router-link to="/mutiTransfer">前往 --> 数据量庞大的分页穿梭框</router-link>
@@ -16,7 +16,7 @@ export default {
   data() {
     return {
       title: '省市级联动多选穿梭框',
-      districtList: {
+      dataObj: {
         province: {
           '101101': '北京市',
           '101102': '天津市',
@@ -35,87 +35,87 @@ export default {
           '101101': [
             {
               id: 101101101112,
-              text: '通州区'
+              label: '通州区'
             },
             {
               id: 101101101111,
-              text: '房山区'
+              label: '房山区'
             }
           ],
           '101102': [
             {
               id: 101102101111,
-              text: '西青区'
+              label: '西青区'
             },
             {
               id: 101102101112,
-              text: '津南区'
+              label: '津南区'
             }
           ],
           '101103': [
             {
               id: 101102101351,
-              text: '河源市'
+              label: '河源市'
             },
             {
               id: 101164001112,
-              text: '惠州市'
+              label: '惠州市'
             },
             {
               id: 101164181112,
-              text: '深圳市'
+              label: '深圳市'
             }
           ]
         },
-        country: {
+        county: {
           '101164181112': [
             {
               id: 106105142126,
-              text: '宝安区'
+              label: '宝安区'
             },
             {
               id: 106105142125,
-              text: '南山区'
+              label: '南山区'
             },
             {
               id: 106105143124,
-              text: '罗湖区'
+              label: '罗湖区'
             },
             {
               id: 106105143125,
-              text: '福田区'
+              label: '福田区'
             }
           ],
           '101102101351': [
             {
               id: 106105143124,
-              text: '龙川县'
+              label: '龙川县'
             },
             {
               id: 106105143125,
-              text: '紫金县'
+              label: '紫金县'
             }
           ],
           '101164001112': [
             {
               id: 106465133124,
-              text: '惠阳区'
+              label: '惠阳区'
             },
             {
               id: 106197987125,
-              text: '惠城区'
+              label: '惠城区'
             }
           ]
         }
       },
-      checkedData: [
+      selectedData: [
         {
           id: '101101-101101101112',
-          text: '北京市-通州区'
+          label: '北京市-通州区'
         },
         {
           id: '101103-101164001112-106197987125',
-          text: '广东省-惠州市-惠城区'
+          label: '广东省-惠州市-惠城区'
         }
       ]
     }
